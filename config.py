543273889f42268fc4a17e9c6e100dd3f17763be
usersushant-beep <imbughunger@gmail.com>
2025-02-28 17:43:18 +0545

    Update config.py

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = API_ID
-API_HASH = "API_HASH"
+API_ID = 26225615
+API_HASH = "286736c51d1964ae919831cfdae5b921"
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "BOT_TOKEN"
+BOT_TOKEN = "7362292376:AAH72kJ5JyrIUKjOT9jmkycSPx68prKCXnQ"
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = "MONGO_DB_URI"
+MONGO_DB_URI = "mongodb+srv://user:passwords@cluster0.0fn1w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = "https://t.me/thanos_pro"
-SUPPORT_GROUP = "https://t.me/thanosprosss"
+SUPPORT_CHANNEL = "https://t.me/skillsdoes"
+SUPPORT_GROUP = "https://t.me/hylop"
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -62,7 +62,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 
 # Get your pyrogram v2 session from Replit
-STRING1 = "STRING_SESSION"
+STRING1 = "1BVtsOGwBu5j0tuGND7yCFYkSBugkRU1DYFON15QXcrlVS7ecAHnlefKYMIvsjzAqqCczyQGBKjYyVdJlQyO2xg2vEDh6IPpXXXOYRNLHS95bCwwX7hDy2ETfL4dTrUP5My7UZCVFBzyGp-Pps5PASCqxiGxXfl85H6w0Y0DOWYfLQK-gMWiiNu4R5GEuCCa66lmg9UUhnHb9fci3V5qGNhMa1Pvwo79vrCXTQeGPtK1tALricKyNVCcmB61ZEbOzn38RFi8pLLhOD1aYxcJ2SFrdoJjbavH7-JSJbFQoj4aJmRkwAfH_mJt5eXYE-AjDhdmqJHUXqSeByLaqOm0V2R5ummYfHZo="
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -77,12 +77,12 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+START_IMG_URL = "https://graph.org/file/4e46692091b4cd35e47fd-dc240cfe77c5038ffc.jpg"
 
-PING_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+PING_IMG_URL = "https://graph.org/file/4e46692091b4cd35e47fd-dc240cfe77c5038ffc.jpg"
 
 PLAYLIST_IMG_URL = "https://graph.org/file/763a841a2ad5cbb1e2fc5.jpg"
-STATS_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+STATS_IMG_URL = "https://graph.org/file/4e46692091b4cd35e47fd-dc240cfe77c5038ffc.jpg"
 TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
 TELEGRAM_VIDEO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
